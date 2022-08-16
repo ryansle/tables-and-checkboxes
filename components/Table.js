99d59c671d38cf295eb3dbfefb251ab7fe945e31
Ryan Le <ryanle@live.com>
2022-08-16 17:43:49 -0400

    feat: ui for table actions (select all/download)

--- components/Table.js
+++ components/Table.js
@@ -2,12 +2,28 @@ import { useState } from 'react';
 
 // Components
 import TableRow from './TableRow';
+import { BsDownload as Download } from 'react-icons/bs';
 
 const Table = ({ data }) => {
   const [selected, setSelected] = useState([]);
 
   return (
     <div>
+      <div className='flex p-2 items-center'>
+        <input
+          type='checkbox'
+        />
+
+        <p className='ml-4'>
+          {selected.length === 0 ? 'None Selected' : `Selected (${selected.length})`}
+        </p>
+
+        <button className='flex items-center ml-10 p-2 rounded hover:bg-gray-200'>
+          <Download className='mr-3' />
+          Download Selected
+        </button>
+      </div>
+
       <table className='border rounded w-full text-left'>
         <thead>
           <tr>
